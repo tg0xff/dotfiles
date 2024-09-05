4c7207408388739ab49d7df98c532040bcf0bbe5
Everardo Tamez Galván <tg0xff@proton.me>
2024-09-04 20:48:23 -0600

    Move org mode module to builtins.el

--- private_dot_config/emacs/builtins.el
+++ private_dot_config/emacs/builtins.el
@@ -64,3 +64,59 @@
   (keymap-global-set "<leader> e" my/eglot-keymap)
   ;; massive perf boost---don't log every event
   (fset #'jsonrpc--log-event #'ignore))
+
+;; https://orgmode.org/
+;; https://git.savannah.gnu.org/cgit/emacs/org-mode.git
+(use-package org
+  :ensure nil
+  :hook
+  (org-mode . variable-pitch-mode)
+  (org-todo-repeat . org-reset-checkbox-state-subtree)
+  (after-init . (lambda () (find-file (concat org-directory "/*.org") t)))
+  :custom-face
+  (org-checkbox ((t (:inherit fixed-pitch))))
+  (org-table ((t (:inherit fixed-pitch))))
+  (org-block ((t (:inherit fixed-pitch))))
+  (org-code ((t (:inherit fixed-pitch))))
+  :custom
+  (org-directory `,(expand-file-name "Documents/org" my/home-directory))
+  (org-agenda-files `(,org-directory))
+  (org-capture-templates '(("d" "default"
+                            entry (file+headline "todo.org" "Inbox")
+                            "* TODO %?" :prepend t)))
+  (org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "WAIT(w)" "|" "DONE(d)")))
+  (org-agenda-window-setup 'other-tab)
+  (org-agenda-tags-column 0)
+  (org-agenda-current-time-string "◀── now ─────────────────────────────────────────────────")
+  (org-M-RET-may-split-line nil)
+  (org-ellipsis "   […]")
+  (org-tags-column 0)
+  (org-log-done 'time)
+  (org-preview-latex-image-directory `,(expand-file-name "org-latex-preview" temporary-file-directory))
+  (org-hide-emphasis-markers t)
+  (org-pretty-entities t)
+  (org-image-actual-width '(400))
+  (org-agenda-custom-commands '(("n" todo "NEXT"
+                                 ((org-agenda-sorting-strategy '(category-up priority-up))))))
+  :config
+  (defvar-keymap my/org-major-mode-keymap
+    :doc "My prefix map for org's major mode."
+    "m" #'my/org-sort-media
+    "t" #'my/org-sort-todo
+    "i" #'org-id-get-create)
+  (defvar-keymap my/org-mode-keymap
+    :doc "My prefix map for org's global commands."
+    "a" #'org-agenda
+    "c" #'org-capture
+    "l" #'org-store-link)
+  (keymap-set global-map "<leader> o" my/org-mode-keymap)
+  (keymap-set org-mode-map "<leader> ." my/org-major-mode-keymap)
+  (defun my/org-sort-media ()
+    (interactive)
+    (org-sort-entries nil ?a)
+    (org-sort-entries nil ?p)
+    (org-sort-entries nil ?o))
+  (defun my/org-sort-todo ()
+    (interactive)
+    (org-sort-entries nil ?p)
+    (org-sort-entries nil ?o)))
