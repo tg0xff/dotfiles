a31873711b70249ae649d113286c5b0ca0bd252b
Everardo Tamez Galván <tg0xff@proton.me>
2024-09-19 18:14:59 -0600

    restic: change level of verbosity

--- private_dot_local/bin/executable_literal_run_restic.sh
+++ private_dot_local/bin/executable_literal_run_restic.sh
@@ -18,6 +18,6 @@ echo "$(date -Ins) Changing directory to \$HOME"
 cd "${HOME}"
 
 echo "$(date -Ins) Making a backup of \$HOME"
-restic --verbose=3 backup . --exclude-file="${HOME}/.config/restic_ignore.txt"
+restic --verbose=2 backup . --exclude-file="${HOME}/.config/restic_ignore.txt"
 
 echo "$(date -Ins) Done running run_restic.sh"
