909f00c0b3e41d25a949ea87eff63afaaa3b9946
Everardo Tamez Galván <tg0xff@proton.me>
2024-10-20 18:15:18 -0600

    emacs: install treesit-auto

--- private_dot_config/emacs/major-modes.el
+++ private_dot_config/emacs/major-modes.el
@@ -2,37 +2,15 @@
 
 ;; ########## Tree-sitter ##########
 
-(setq treesit-language-source-alist '((bash "https://github.com/tree-sitter/tree-sitter-bash")
-                                      (c "https://github.com/tree-sitter/tree-sitter-c")
-                                      (c++ "https://github.com/tree-sitter/tree-sitter-cpp")
-                                      (css "https://github.com/tree-sitter/tree-sitter-css")
-                                      (javascript "https://github.com/tree-sitter/tree-sitter-javascript")
-                                      (json "https://github.com/tree-sitter/tree-sitter-json")
-                                      (python "https://github.com/tree-sitter/tree-sitter-python")
-                                      (rust "https://github.com/tree-sitter/tree-sitter-rust")
-                                      (toml "https://github.com/ikatyang/tree-sitter-toml")
-                                      (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
-                                      (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
-                                      (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
-
-(setq treesit-load-name-override-list '((c++ "libtree-sitter-c++" "tree_sitter_cpp")))
-
-(cl-loop for (key . value) in treesit-language-source-alist
-         do (unless (treesit-language-available-p key)
-              (treesit-install-language-grammar key)))
-
-(custom-set-variables '(major-mode-remap-alist '((bash-mode . bash-ts-mode)
-                                                 (c-mode . c-ts-mode)
-                                                 (c++-mode . c++-ts-mode)
-                                                 (css-mode . css-ts-mode)
-                                                 (javascript-mode . js-ts-mode)
-                                                 (json-mode . json-ts-mode)
-                                                 (python-mode . python-ts-mode)
-                                                 (rust-mode . rust-ts-mode)
-                                                 (toml-mode . toml-ts-mode)
-                                                 (tsx-mode . tsx-ts-mode)
-                                                 (typescript-mode . typescript-ts-mode)
-                                                 (yaml-mode . yaml-ts-mode))))
+;; Automatic installation, usage, and fallback for tree-sitter major
+;; modes in Emacs 29
+;; https://github.com/renzmann/treesit-auto
+(use-package treesit-auto
+  :custom
+  (treesit-auto-install 'prompt)
+  :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode))
 
 ;; ########## Ledger ##########
 
