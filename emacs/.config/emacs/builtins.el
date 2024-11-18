67be246b85eb5709b4257d6fe9e2821aea0f7d87
Everardo Tamez Galván <tg0xff@proton.me>
2024-11-17 23:16:06 -0600

    emacs: simplify next agenda view

--- emacs/.config/emacs/builtins.el
+++ emacs/.config/emacs/builtins.el
@@ -56,8 +56,7 @@
   (org-log-done 'time)
   (org-preview-latex-image-directory `,(expand-file-name "org-latex-preview" temporary-file-directory))
   (org-image-actual-width '(400))
-  (org-agenda-custom-commands '(("n" todo "NEXT"
-                                 ((org-agenda-sorting-strategy '(category-up priority-down))))
+  (org-agenda-custom-commands '(("n" todo "NEXT")
                                 ("c" tags "+@casa")
                                 ("j" tags "+@trabajo")
                                 ("d" tags "+@tienda")))
