2155507970ee99d44dd48733f8ba11872327db91
Everardo Tamez Galván <tg0xff@proton.me>
2024-11-23 12:41:25 -0600

    Add encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -8,17 +8,18 @@ from os.path import isfile
 def main():
     for fi in iglob("**/*.age", root_dir=".", recursive=True, include_hidden=True):
         decrypted_file = fi[:-4]
-        if not isfile(fi) or fi == "key.txt.age":
+        if not isfile(decrypted_file) or fi == "key.txt.age":
             continue
         call(
             [
                 "age",
-                "--decrypt",
-                "--identity",
-                "key.txt",
+                "--encrypt",
+                "--armor",
+                "--recipient",
+                "age1ew8vjkz2qyt23yg9h4kwnesl9307ffspr05yyj6qwrwtaul9wp8q8emwrd",
                 "--output",
-                decrypted_file,
                 fi,
+                decrypted_file,
             ]
         )
 
