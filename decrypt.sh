250ac76244000720cb1d6499fc64170e2f4caee3
Everardo Tamez Galván <tg0xff@proton.me>
2024-11-25 11:18:41 -0600

    Make commands more verbose

--- decrypt.sh
+++ decrypt.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd "${HOME}/.dotfiles/"
-fd --hidden --type f --extension age --exclude key.txt.age -x age --decrypt --identity key.txt --output "{.}" "{}"
+fd --hidden --type f --extension age --exclude key.txt.age --exec age --decrypt --identity key.txt --output "{.}" "{}"
--- encrypt.sh
+++ encrypt.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd "${HOME}/.dotfiles/"
-fd --hidden --type f --extension age --exclude key.txt.age -x age --encrypt --armor --recipient age1ew8vjkz2qyt23yg9h4kwnesl9307ffspr05yyj6qwrwtaul9wp8q8emwrd --output "{}" "{.}"
+fd --hidden --type f --extension age --exclude key.txt.age --exec age --encrypt --armor --recipient age1ew8vjkz2qyt23yg9h4kwnesl9307ffspr05yyj6qwrwtaul9wp8q8emwrd --output "{}" "{.}"
