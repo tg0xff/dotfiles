9885eb025b5eb53ed811ede71ee696035874b759
Everardo Tamez Galván <tg0xff@proton.me>
2024-12-11 16:38:54 -0600

    Add firefox config

---
+++ nostow-firefox/user.js
@@ -0,0 +1,6 @@
+user_pref("signon.rememberSignons", false);
+user_pref("browser.urlbar.suggest.topsites", false);
+// Allow userChrome.css
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+user_pref("general.smoothScroll", false);
