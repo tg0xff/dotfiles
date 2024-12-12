37d57e498104aba7e296dfcc3fa4bab2bf2270ef
Everardo Tamez Galván <tg0xff@proton.me>
2024-12-11 23:09:54 -0600

    scripts: turn simple scripts into functions

--- shells/.shared.sh
+++ shells/.shared.sh
@@ -20,3 +20,15 @@ alias e='emacsclient --alternate-editor emacs --tty'
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
+git-gh-deploy() {
+    if [ -z "$1" ]; then
+        echo "Which folder do you want to deploy to GitHub Pages?"
+        exit 1
+    fi
+    git subtree push --prefix $1 origin gh-pages
+}
+
+mirror-website() {
+    wget --mirror --convert-links --adjust-extension --page-requisites --no-parent --timestamping $@
+}
