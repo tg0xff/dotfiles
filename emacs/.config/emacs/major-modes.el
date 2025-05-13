7c1e61c84551a4e5c2aa66e85cd5abeb3a26f093
Everardo Tamez Galván <tg0xff@proton.me>
2025-05-12 18:15:17 -0600

    emacs: delete header comments

--- emacs/.config/emacs/major-modes.el
+++ emacs/.config/emacs/major-modes.el
@@ -1,7 +1,5 @@
 ;; -*- lexical-binding: t -*-
 
-;; ########## Tree-sitter ##########
-
 ;; Automatic installation, usage, and fallback for tree-sitter major
 ;; modes in Emacs 29
 ;; https://github.com/renzmann/treesit-auto
@@ -10,20 +8,14 @@
     :config
     (global-treesit-auto-mode)))
 
-;; ########## Lua ##########
-
 ;; https://github.com/immerrr/lua-mode
 (use-package lua-mode
   :defer t)
-
-;; ########## Markdown ##########
 
 ;; https://github.com/jrblevin/markdown-mode
 (use-package markdown-mode
   :defer t
   :hook (markdown-mode . variable-pitch-mode))
-
-;; ########## Web development ##########
 
 (custom-set-variables '(css-indent-offset 2)
                       '(js-indent-level 2))
