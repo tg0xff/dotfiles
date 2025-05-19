b0803f08f268784dea153fa3b1f4791b4e703688
Everardo Tamez Galván <tg0xff@proton.me>
2025-05-19 15:39:54 -0600

    neovim: modularise config

---
+++ neovim/.config/nvim/lua/custom/plugins/nvim-lint.lua
@@ -0,0 +1,38 @@
+return {
+    'mfussenegger/nvim-lint',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+        local lint = require 'lint'
+        -- This way of setting linters_by_ft allows other plugins to add linters
+        -- to require('lint').linters_by_ft.
+        lint.linters_by_ft = lint.linters_by_ft or {}
+        lint.linters_by_ft['markdown'] = { 'markdownlint' }
+
+        -- Disable the default linters.
+        lint.linters_by_ft['clojure'] = nil
+        lint.linters_by_ft['dockerfile'] = nil
+        lint.linters_by_ft['inko'] = nil
+        lint.linters_by_ft['janet'] = nil
+        lint.linters_by_ft['json'] = nil
+        lint.linters_by_ft['markdown'] = nil
+        lint.linters_by_ft['rst'] = nil
+        lint.linters_by_ft['ruby'] = nil
+        lint.linters_by_ft['terraform'] = nil
+        lint.linters_by_ft['text'] = nil
+
+        -- Create autocommand which carries out the actual linting
+        -- on the specified events.
+        local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+        vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+            group = lint_augroup,
+            callback = function()
+                -- Only run the linter in buffers that you can modify in order to
+                -- avoid superfluous noise, notably within the handy LSP pop-ups that
+                -- describe the hovered symbol using Markdown.
+                if vim.bo.modifiable then
+                    lint.try_lint()
+                end
+            end,
+        })
+    end,
+}
