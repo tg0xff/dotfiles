400d698efff55d667f3ef7f5b6614574bd5fcc65
Everardo Tamez Galván <tg0xff@proton.me>
2025-06-05 08:22:07 -0600

    neovim: change files and grep telescope keymaps
    Now s[fg] search within projects; s[FG] search in HOME; and s<A-[fg]>
    search including hidden files in HOME.

--- neovim/.config/nvim/lua/custom/plugins/telescope.lua
+++ neovim/.config/nvim/lua/custom/plugins/telescope.lua
@@ -34,20 +34,6 @@ return {
         pcall(require('telescope').load_extension, 'ui-select')
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-        vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-        vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-        vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-        vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-        vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-        vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-
-        vim.keymap.set('n', '<leader>/', function()
-            builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({
-                winblend = 10,
-                previewer = false,
-            }))
-        end, { desc = '[/] Fuzzily search in current buffer' })
 
         -- Returns project root dir or CWD if there isn't one.
         -- It returns $HOME if there's no CWD either.
@@ -55,20 +41,49 @@ return {
             return require('mini.misc').find_root() or vim.uv.cwd() or '$HOME'
         end
 
-        vim.keymap.set('n', '<leader>s/', function()
-            builtin.live_grep({
-                cwd = find_project_root(),
-                additional_args = { '--hidden' },
-                prompt_title = 'Live Grep in CWD',
-            })
-        end, { desc = '[S]earch [/] by grep in project dir.' })
-
-        vim.keymap.set('n', '<leader>s.', function()
+        vim.keymap.set('n', '<leader>sf', function()
             builtin.find_files({
                 cwd = find_project_root(),
                 hidden = true,
                 prompt_title = 'Find Project Files',
             })
-        end, { desc = '[S]earch [.] project files' })
+        end, { desc = '[S]earch project [F]iles' })
+
+        vim.keymap.set('n', '<leader>sg', function()
+            builtin.live_grep({
+                cwd = find_project_root(),
+                additional_args = { '--hidden' },
+                prompt_title = 'Live Grep in Project',
+            })
+        end, { desc = '[S]earch by [G]rep in project' })
+
+        vim.keymap.set('n', '<leader>s<A-f>', function()
+            builtin.find_files({
+                hidden = true,
+                prompt_title = 'Find All Files',
+            })
+        end, { desc = '[S]earch all [A-f]iles' })
+
+        vim.keymap.set('n', '<leader>s<A-g>', function()
+            builtin.find_files({
+                additional_args = { '--hidden' },
+                prompt_title = 'Live Grep All',
+            })
+        end, { desc = '[S]earch all by [A-g]rep' })
+
+        vim.keymap.set('n', '<leader>sF', builtin.find_files, { desc = '[S]earch [F]iles' })
+        vim.keymap.set('n', '<leader>sG', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+        vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+        vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+        vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+        vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+        vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+
+        vim.keymap.set('n', '<leader>/', function()
+            builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({
+                winblend = 10,
+                previewer = false,
+            }))
+        end, { desc = '[/] Fuzzily search in current buffer' })
     end,
 }
