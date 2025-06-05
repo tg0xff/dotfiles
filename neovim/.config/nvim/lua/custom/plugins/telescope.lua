94fff41393b2039d59c86d2c0fba88fbc0d9a009
Everardo Tamez Galván <tg0xff@proton.me>
2025-06-05 03:20:42 -0600

    neovim: delete <leader>sw

--- neovim/.config/nvim/lua/custom/plugins/telescope.lua
+++ neovim/.config/nvim/lua/custom/plugins/telescope.lua
@@ -38,7 +38,6 @@ return {
         vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
         vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
         vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-        vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
         vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
         vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
         vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
